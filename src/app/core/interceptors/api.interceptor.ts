5ef0c5d4624acb513e867d4824cc329ddfde630b
JamshidGuliston <jamshidstarx@gmail.com>
2026-03-08 20:36:50 +0500

    intercep

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -2,11 +2,24 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  let apiReq = req;
+
+  // 1. Base URL qo'shish
   if (req.url.startsWith('/')) {
-    const apiReq = req.clone({
-      url: `${environment.apiUrl}${req.url}`
-    });
-    return next(apiReq);
+    apiReq = apiReq.clone({ url: `${environment.apiUrl}${req.url}` });
   }
-  return next(req);
+
+  // 2. Authorization token qo'shish
+  const teacherData = localStorage.getItem('teacher');
+  if (teacherData) {
+    const teacher = JSON.parse(teacherData);
+    const token = teacher?.api_token;
+    if (token) {
+      apiReq = apiReq.clone({
+        headers: apiReq.headers.set('Authorization', `Token ${token}`)
+      });
+    }
+  }
+
+  return next(apiReq);
 };
